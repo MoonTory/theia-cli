d18a04144c847c2c81d5155af1d5a91a681cd2c9
MoonTory <gustavoqnt40@gmail.com>
2019-11-24 22:44:08 -0300

    chore(#4): Configure Testing
    Mocha & Chai
    nyc test coverage
    
    closes #4

---
+++ tests/cli/parseArgsIntoOptions.test.ts
@@ -0,0 +1,9 @@
+import { describe, it } from 'mocha';
+import { expect } from 'chai';
+
+describe('calculate', function() {
+	it('add', function() {
+		let result = 5 + 2;
+		expect(result).equal(7);
+	});
+});
